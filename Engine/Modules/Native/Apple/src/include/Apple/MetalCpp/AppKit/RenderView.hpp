e58650b644b4224e7443c7932261b508217d5c69
Catalin Chirosca <catalin270402@gmail.com>
2026-05-26 15:42:36 +0200

    Update: Enhance RenderView class with additional initialization methods and rendering properties

--- Engine/Modules/Native/Apple/src/include/Apple/MetalCpp/AppKit/RenderView.hpp
+++ Engine/Modules/Native/Apple/src/include/Apple/MetalCpp/AppKit/RenderView.hpp
@@ -16,82 +16,205 @@
 #include "Apple/MetalCpp/Foundation/NSObject.hpp"
 #include "Apple/Types/EventHandlers/I_ViewEventHandler.hpp"
 
+#include <CoreGraphics/CGColorSpace.h>
+
 
 namespace CA {
-class MetalLayer;
+class MetalDrawable;
 }
 namespace MTL {
+class Device;
 class RenderPassDescriptor;
 }
 
 namespace NS {
 
+/**
+ * @class RenderView
+ * @brief A view for rendering Metal content in a macOS application
+ * @details This class represents a view that can be used for rendering Metal content in a macOS application. It provides methods for managing the view's properties, handling events, and accessing the current render pass descriptor for rendering operations. Clients can use this class to create and configure a view that is suitable for Metal rendering, as well as to handle user interactions and other events related to the view.
+ */
 class RenderView: public Referencing<RenderView> {
 public:
+	/**
+	 * @brief Allocates a new instance of RenderView
+	 * @return RenderView* Pointer to the newly allocated RenderView instance
+	 * @details The alloc method is a static factory method that allocates a new instance of RenderView. Clients should call this method to create a new view before initializing it with init() or initWithCoder().
+	 */
 	static RenderView* alloc();
-	[[nodiscard]] RenderView* init(const CGRect& frame) const;
+
+	/**
+	 * @brief Initializes the RenderView instance
+	 * @param frame The frame rectangle for the view, measured in points. The origin of the frame is relative to the superview's coordinate system.
+	 * @param device A pointer to the MTLDevice that will be used for rendering. This device will be associated with the RenderView and used for rendering operations.
+	 * @return RenderView* Pointer to the initialized RenderView instance
+	 * @details The init method initializes the RenderView instance with a specified frame and Metal device.
+	 *			Clients should call this method after allocating a new instance with alloc() to properly initialize the view with the desired frame and rendering device.
+	 *			The provided frame will determine the size and position of the view, while the Metal device will be used for rendering operations.
+	 *			Clients should ensure that the Metal device is properly initialized and compatible with the RenderView for successful rendering.
+	 */
+	[[nodiscard]] RenderView* init(const CGRect& frame, const MTL::Device* device);
+
+	/**
+	 * @brief Initializes the RenderView instance with a coder
+	 * @param coder A pointer to the Coder object used for initialization
+	 * @return RenderView* Pointer to the initialized RenderView instance
+	 * @details The initWithCoder method initializes the RenderView instance using a Coder object. This is typically used when the view is being loaded from a storyboard or nib file. Clients should call this method after allocating a new instance with alloc() if they are initializing the view from a coder.
+	 */
+	[[nodiscard]] RenderView* init(const Coder* coder);
 
 public:
+	[[nodiscard]] CGPoint convertPointFromView(const CGPoint& point);
+	[[nodiscard]] CGPoint convertPointFromView(const CGPoint& point, const RenderView* view);
+
+	void releaseDrawables();
+
+	void draw();
+
+public:
+	void setAutoresizeDrawable(bool autoresizeDrawable);
+
+	void setClearColor(const MTL::ClearColor& clearColor);
+
+	void setClearDepth(double clearDepth);
+
+	void setClearStencil(uint32_t clearStencil);
+
+	void setColorPixelFormat(MTL::PixelFormat colorPixelFormat);
+
+	void setColorSpace(CGColorSpaceRef colorSpace);
+
+	void SetDelegate(const void* delegate);
+
+	void setDepthStencilAttachmentTextureUsage(MTL::TextureUsage textureUsage);
+
+	void setDepthStencilPixelFormat(MTL::PixelFormat colorPixelFormat);
+
+	void SetDevice(const MTL::Device* device);
+
+	void setDrawableSize(CGSize drawableSize);
+
+	void setEnableSetNeedsDisplay(bool enableSetNeedsDisplay);
+
+	void SetFramebufferOnly(bool framebufferOnly);
+
+	void SetEventHandler(I_ViewEventHandler* handler);
+
+	void setLayer(CA::MetalLayer* layer);
+
+	void setMultisampleColorAttachmentTextureUsage(MTL::TextureUsage textureUsage);
+
+	void setOpaque(bool opaque);
+
+	void setSampleCount(UInteger sampleCount);
+
+	void setPaused(bool paused);
+
+	void setPreferredFramesPerSecond(Integer preferredFramesPerSecond);
+
+	void setPresentsWithTransaction(bool presentsWithTransaction);
+
+	void setWantsLayer(bool wantsLayer);
+
+public:
+	[[nodiscard]] bool autoresizeDrawable() const;
+
+	[[nodiscard]] MTL::ClearColor clearColor() const;
+
+	[[nodiscard]] double clearDepth() const;
+
+	[[nodiscard]] uint32_t clearStencil() const;
+
+	[[nodiscard]] CGColorSpaceRef colorSpace() const;
+
+	[[nodiscard]] MTL::PixelFormat colorPixelFormat() const;
+
+	[[nodiscard]] CA::MetalDrawable* currentDrawable() const;
+
 	[[nodiscard]] MTL::RenderPassDescriptor* currentRenderPassDescriptor() const;
 
-public:
-	[[nodiscard]] CGPoint convertPointFromView(const CGPoint& point) const;
-	[[nodiscard]] CGPoint convertPointFromView(const CGPoint& point, const RenderView* view) const;
+	[[nodiscard]] MTL::TextureUsage depthStencilAttachmentTextureUsage() const;
 
-public:
-	void SetEventHandler(I_ViewEventHandler* handler) const;
-	void setLayer(CA::MetalLayer* layer) const;
-	void setOpaque(bool opaque) const;
-	void setWantsLayer(bool wantsLayer) const;
+	[[nodiscard]] MTL::PixelFormat depthStencilPixelFormat() const;
 
-public:
+	[[nodiscard]] MTL::Texture* depthStencilTexture() const;
+
+	[[nodiscard]] void* delegate() const;
+
+	[[nodiscard]] MTL::Device* device() const;
+
+	[[nodiscard]] CGSize drawableSize() const;
+
+	[[nodiscard]] bool enableSetNeedsDisplay() const;
+
+	[[nodiscard]] bool framebufferOnly() const;
+
 	[[nodiscard]] CGRect frame() const;
-	[[nodiscard]] void* getUserData() const;
+
+	[[nodiscard]] bool isPaused() const;
+
 	[[nodiscard]] void* layer() const;
+
+	[[nodiscard]] MTL::TextureUsage multisampleColorAttachmentTextureUsage() const;
+
+	[[nodiscard]] MTL::Texture* multisampleColorTexture() const;
+
+	[[nodiscard]] UInteger sampleCount() const;
+
+	[[nodiscard]] MTL::Device* preferredDevice() const;
+
+	[[nodiscard]] CGSize preferredDrawableSize() const;
+
+	[[nodiscard]] Integer preferredFramesPerSecond() const;
+
+	[[nodiscard]] bool presentsWithTransaction() const;
 };
 
 _NS_INLINE RenderView* RenderView::alloc() {
 	return sendMessage<RenderView*>(_APPKIT_PRIVATE_CLS(RenderView), _APPKIT_PRIVATE_SEL(alloc));
 }
 
-_NS_INLINE RenderView* RenderView::init(const CGRect& frame) const {
-	return sendMessage<RenderView*>(this, _APPKIT_PRIVATE_SEL(initWithFrame_), frame);
+// ReSharper disable once CppMemberFunctionMayBeConst
+_NS_INLINE RenderView* RenderView::init(const CGRect& frame, const MTL::Device* device) {
+	return sendMessage<RenderView*>(this, _APPKIT_PRIVATE_SEL(initWithFrame_), frame, device);
 }
 
 _NS_INLINE MTL::RenderPassDescriptor* RenderView::currentRenderPassDescriptor() const {
 	return sendMessage<MTL::RenderPassDescriptor*>(this, _APPKIT_PRIVATE_SEL(currentRenderPassDescriptor));
 }
 
-_NS_INLINE CGPoint RenderView::convertPointFromView(const CGPoint& point) const {
+// ReSharper disable once CppMemberFunctionMayBeConst
+_NS_INLINE CGPoint RenderView::convertPointFromView(const CGPoint& point) {
 	return sendMessage<CGPoint>(this, _APPKIT_PRIVATE_SEL(convertPoint_fromView_), point, this);
 }
 
-_NS_INLINE CGPoint RenderView::convertPointFromView(const CGPoint& point, const RenderView* view) const {
+// ReSharper disable once CppMemberFunctionMayBeConst
+_NS_INLINE CGPoint RenderView::convertPointFromView(const CGPoint& point, const RenderView* view) {
 	return sendMessage<CGPoint>(this, _APPKIT_PRIVATE_SEL(convertPoint_fromView_), point, view);
 }
 
-_NS_INLINE void RenderView::SetEventHandler(I_ViewEventHandler* handler) const {
+// ReSharper disable once CppMemberFunctionMayBeConst
+_NS_INLINE void RenderView::SetEventHandler(I_ViewEventHandler* handler) {
 	return sendMessage<void>(this, _APPKIT_PRIVATE_SEL(setEventHandler_), handler);
 }
 
-_NS_INLINE void RenderView::setLayer(CA::MetalLayer* layer) const {
+// ReSharper disable once CppMemberFunctionMayBeConst
+_NS_INLINE void RenderView::setLayer(CA::MetalLayer* layer) {
 	return sendMessage<void>(this, _APPKIT_PRIVATE_SEL(setLayer_), layer);
 }
 
-_NS_INLINE void RenderView::setOpaque(const bool opaque) const {
+// ReSharper disable once CppMemberFunctionMayBeConst
+_NS_INLINE void RenderView::setOpaque(const bool opaque) {
 	return sendMessage<void>(this, _APPKIT_PRIVATE_SEL(setOpaque_), opaque);
 }
 
-_NS_INLINE void RenderView::setWantsLayer(const bool wantsLayer) const {
+// ReSharper disable once CppMemberFunctionMayBeConst
+_NS_INLINE void RenderView::setWantsLayer(const bool wantsLayer) {
 	return sendMessage<void>(this, _APPKIT_PRIVATE_SEL(setWantsLayer_), wantsLayer);
 }
 
 _NS_INLINE CGRect RenderView::frame() const {
 	return sendMessage<CGRect>(this, _APPKIT_PRIVATE_SEL(frame));
-}
-
-_NS_INLINE void* RenderView::getUserData() const {
-	return sendMessage<void*>(this, _APPKIT_PRIVATE_SEL(getUserData_));
 }
 
 _NS_INLINE void* RenderView::layer() const {
