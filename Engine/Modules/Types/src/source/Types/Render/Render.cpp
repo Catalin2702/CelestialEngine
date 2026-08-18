077e26e2b67e0efc6284c475ae01d40b1a444460
Catalin Chirosca <catalin270402@gmail.com>
2026-08-18 18:33:17 +0200

    Removed Vulkan from MacOS supported graphics api

--- Engine/Modules/Types/src/source/Types/Render/Render.cpp
+++ Engine/Modules/Types/src/source/Types/Render/Render.cpp
@@ -4,7 +4,7 @@
 // Created by: Catalin Chirosca
 // Created: 2026-03-19
 // Updated by: Catalin Chirosca
-// Updated: 2026-07-13
+// Updated: 2026-08-18
 //
 
 #include "Types/Render/Render.hpp"
@@ -15,17 +15,18 @@ bool IsGraphicsApiSupported(const GraphicsApi& api) {
 	if (api == GraphicsApi::None)
 		return false;
 
-	if (api == GraphicsApi::OpenGL or api == GraphicsApi::Vulkan)
+	if (api == GraphicsApi::OpenGL)
 		return true;
 
 #ifdef CE_PLATFORM_MACOS
 	if (api == GraphicsApi::Metal)
 		return true;
 #elifdef CE_PLATFORM_WINDOWS
-	if (api == GraphicsApi::DirectX11 or api == GraphicsApi::DirectX12)
+	if (api == GraphicsApi::DirectX11 or api == GraphicsApi::DirectX12  or api == GraphicsApi::Vulkan)
 		return true;
 #elifdef CE_PLATFORM_LINUX
-	// DirectX is not supported on Linux, and Metal is not supported on Linux
+	if (api == GraphicsApi::Vulkan)
+		return true;
 #else
 		#error Unsupported platform for graphics API support check
 #endif
