aa4ce007a85d3e7509da5d0152c6e09a92914ec0
Catalin Chirosca <catalin270402@gmail.com>
2026-09-04 14:28:38 +0200

    Changing method order

--- Engine/Modules/Core/src/interface/Core/Render/Command/I_CommandEncoder.hpp
+++ Engine/Modules/Core/src/interface/Core/Render/Command/I_CommandEncoder.hpp
@@ -4,7 +4,7 @@
 // Created by: Catalin Chirosca
 // Created: 2026-08-29
 // Updated by: Catalin Chirosca
-// Updated: 2026-08-30
+// Updated: 2026-09-04
 //
 
 #pragma once
@@ -29,14 +29,14 @@ public:
 	virtual ~I_CommandEncoder() = default;
 
 public:
+	virtual void DrawIndexed(u32 indexCount, u32 firstIndex, u32 baseVertex) = 0;
+	virtual void End() = 0;
+
+public:
 	virtual void SetPipelineState(const I_PipelineState& pipelineState) = 0;
 	virtual void SetIndexBuffer(const I_IndexBuffer& indexBuffer) = 0;
 	virtual void SetVertexBuffer(const I_VertexBuffer& vertexBuffer) = 0;
 	virtual void SetViewport(const Viewport& viewport) = 0;
-
-public:
-	virtual void DrawIndexed(u32 indexCount, u32 firstIndex, u32 baseVertex) = 0;
-	virtual void End() = 0;
 
 public:
 	[[nodiscard]] virtual Types::GraphicsApi GetGraphicApi() const = 0;
