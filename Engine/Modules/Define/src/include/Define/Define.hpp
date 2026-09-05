f3551e427e18a7faeeb97efddd35115740ece72d
Catalin Chirosca <catalin270402@gmail.com>
2026-09-05 22:06:09 +0200

    Added Define file that include all the files in the module

---
+++ Engine/Modules/Define/src/include/Define/Define.hpp
@@ -0,0 +1,23 @@
+//
+// Module: CelestialEngine/Engine/Modules/Define
+// File: Define.hpp
+// Created by: Catalin Chirosca
+// Created: 2026-09-05
+// Updated by: Catalin Chirosca
+// Updated: 2026-09-05
+//
+
+#pragma once
+
+#ifndef CE_DEFINE_DEFINE_HPP
+#define CE_DEFINE_DEFINE_HPP
+
+#include "Bind.hpp"
+#include "Config.hpp"
+#include "DynamicLinker.hpp"
+#include "Event.hpp"
+#include "Log.hpp"
+#include "Render.hpp"
+#include "Types.hpp"
+
+#endif //CE_DEFINE_DEFINE_HPP
